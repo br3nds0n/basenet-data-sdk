a3bfedef07b56e540b6ecb6870254cfc4adf37fb
Brendson Silva <brendson.net@gmail.com>
2024-02-17 23:12:21 -0300

    feat: create base entity

---
+++ BaseNet.Data.SDK/Base/EntityBase.cs
@@ -0,0 +1,10 @@
+namespace BaseNet.Data.SDK.Base
+{
+    public class EntityBase
+    {
+        public string? CreatedUser { get; set; }
+        public string? UpdatedUser { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
